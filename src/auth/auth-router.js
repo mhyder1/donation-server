8fcb19e483bd4f344938c0a6d38e50ab1302f73c
Julia Bugayev <jbugayev18@gmail.com>
2020-10-14 09:26:12 -0700

    Added middleware folder with error-handler and jwt-auth

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -3,3 +3,18 @@ const AuthService = require("./auth-service");
 
 const authRouter = express.Router();
 const jsonBodyParser = express.json();
+
+authRouter 
+    .route('/token')
+    .post(jsonBodyParser, async (req, res, next) => {
+        const { username, password } = req.body 
+        const loginUser = { username, password }
+
+        for (const [key, value] of Object.entries(loginUser))
+            if(value == null)
+                return res.status(400).json({
+                    error: `Missing '${key} in request body`
+                })
+    })
+
+    
