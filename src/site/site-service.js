06de9b3c540f441ef340f50857425057bbc49e9c
John Adney <jmoon5ftm@gmail.com>
2020-10-15 10:50:50 -0700

    add as-yet-untested box query implementation --j

---
+++ src/site/site-service.js
@@ -0,0 +1,8 @@
+const boxQuery = "box(point(lat, lon), point(lat, lon)) && '(?, ?), (?, ?)'::box";
+
+module.exports = {
+  // TODO -- do something about the seam at +/-180 degrees longitude
+  getSitesInWindow(db, leftLon, topLat, rightLon, bottomLat) {
+    return db("site").whereRaw(boxQuery, [leftLon, topLat, rightLon, bottomLat]);
+  }
+};
